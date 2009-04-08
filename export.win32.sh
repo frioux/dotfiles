0cec8a1817001148525170ef5318b54bdd63aa69
Frew Schmidt <frioux@gmail.com>
2009-04-08 10:32:46 -0500

    win32 export/import tools

---
+++ export.win32.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp vimrc  ~/_vimrc
+rm ~/vimfiles -R
+cp vim ~/vimfiles -R
+cp zshrc ~/.zshrc
+cp screenrc ~/.screenrc
+
---
+++ import.win32.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ~/_vimrc vimrc
+rm vim -R
+cp ~/vimfiles vim -R
+cp ~/.zshrc zshrc
+cp ~/.screenrc screenrc
+
