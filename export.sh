95d6a4b44c83c318898ebadc09933816a727b60b
Arthur Axel "fREW" Schmidt <frioux@gmail.com>
2009-04-08 20:44:42 -0500

    better import export scripts

--- export.sh
+++ export.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cp zshrc ~/.zshrc
-cp vimrc ~/.vimrc
-cp screenrc ~/.screenrc
-rm ~/.vim -R
-cp vim ~/.vim -R
+rm ~/.vim ~/.zshrc ~/.vimrc ~/.screenrc ~/.irssi -R
+ln -s "$(pwd)/zshrc" ~/.zshrc
+ln -s "$(pwd)/vimrc" ~/.vimrc
+ln -s "$(pwd)/screenrc" ~/.screenrc
+ln -s "$(pwd)/vim" ~/.vim
+ln -s "$(pwd)/irssi" ~/.irssi
 
--- import.sh
+++ import.sh
@@ -5,4 +5,6 @@ cp ~/.vimrc vimrc
 cp ~/.screenrc screenrc
 rm -r vim
 cp ~/.vim vim -R
+rm -r irssi
+cp ~/.irssi irssi -R
 
