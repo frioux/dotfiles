1e4e0261121d92bed585461179f3436e88e48622
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-11-04 00:11:21 -0600

    more stuff to work off of

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,19 @@
 import XMonad
+
+import XMonad.ManageHook
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.UrgencyHook
 import qualified XMonad.StackSet as W
+
+{- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-UrgencyHook.html -}
+import XMonad.Hooks.UrgencyHook
+
+{- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html -}
+import XMonad.Util.EZConfig
+
+{- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-Dzen.html -}
+import XMonad.Util.Dzen
+
 
 main = xmonad $ withUrgencyHook dzenUrgencyHook {
                   args = ["-bg", "darkgreen", "-xs", "1"]
@@ -12,3 +23,9 @@ main = xmonad $ withUrgencyHook dzenUrgencyHook {
                   layoutHook = ewmhDesktopsLayout $ avoidStruts
                                                   $ layoutHook defaultConfig
             }
+            `additionalKeys`
+                 [
+                  ((mod1Mask, xK_y        ), dzen "Hi, mom!" 5000000),
+                  ((mod1Mask, xK_m        ), spawn "xterm"),
+                  ((mod1Mask, xK_x        ), spawn "/home/frew/bin/showdm")
+                 ]
