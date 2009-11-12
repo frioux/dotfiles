5d5d11a7a2510f980a1209f49446a55db3d73497
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-11-11 23:13:50 -0600

    terminator conifg

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
+
+if [[ ! -x ~/bin ]]; then
+   mkdir ~/bin
+fi
+
+if [[ ! -x ~/.config/terminator ]]; then
+   mkdir -p ~/.config/terminator
+fi
 
 rm ~/.xmonad ~/bin/showdm ~/bin/eval ~/.Xdefaults ~/.vim ~/.zshrc ~/.vimrc\
-   ~/.screenrc ~/.irssi -R
+   ~/.screenrc ~/.irssi ~/.config/terminator/config -R
 ln -s "$(pwd)/zshrc" ~/.zshrc
 ln -s "$(pwd)/bin/showdm" ~/bin/showdm
 ln -s "$(pwd)/bin/eval" ~/bin/eval
@@ -11,4 +19,5 @@ ln -s "$(pwd)/screenrc" ~/.screenrc
 ln -s "$(pwd)/vim" ~/.vim
 ln -s "$(pwd)/irssi" ~/.irssi
 ln -s "$(pwd)/xmonad" ~/.xmonad
+ln -s "$(pwd)/terminator_config" ~/.config/terminator/config
 
