afeedd814f7e21c9113d22f89a4f4e172925a364
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-11-23 23:02:01 -0600

    add git config for colors and aliases

--- install.sh
+++ install.sh
@@ -9,15 +9,16 @@ if [[ ! -x ~/.config/terminator ]]; then
 fi
 
 rm ~/.xmonad ~/bin/showdm ~/bin/eval ~/.Xdefaults ~/.vim ~/.zshrc ~/.vimrc\
-   ~/.screenrc ~/.irssi ~/.config/terminator/config -R
-ln -s "$(pwd)/zshrc" ~/.zshrc
-ln -s "$(pwd)/bin/showdm" ~/bin/showdm
+   ~/.screenrc ~/.irssi ~/.gitconfig ~/.config/terminator/config -R
 ln -s "$(pwd)/bin/eval" ~/bin/eval
-ln -s "$(pwd)/Xdefaults" ~/.Xdefaults
-ln -s "$(pwd)/vimrc" ~/.vimrc
-ln -s "$(pwd)/screenrc" ~/.screenrc
-ln -s "$(pwd)/vim" ~/.vim
+ln -s "$(pwd)/bin/showdm" ~/bin/showdm
 ln -s "$(pwd)/irssi" ~/.irssi
-ln -s "$(pwd)/xmonad" ~/.xmonad
+ln -s "$(pwd)/gitconfig" ~/.gitconfig
+ln -s "$(pwd)/screenrc" ~/.screenrc
 ln -s "$(pwd)/terminator_config" ~/.config/terminator/config
+ln -s "$(pwd)/vimrc" ~/.vimrc
+ln -s "$(pwd)/vim" ~/.vim
+ln -s "$(pwd)/Xdefaults" ~/.Xdefaults
+ln -s "$(pwd)/xmonad" ~/.xmonad
+ln -s "$(pwd)/zshrc" ~/.zshrc
 
