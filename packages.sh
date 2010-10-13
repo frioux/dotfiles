d50acfa8c331863b2838b8e79c4aa34d7bb5ddd4
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-10-12 22:07:37 -0500

    install openvpn too

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aptitude install git-core gitg gitk git-doc zsh firefox xmonad dwm-tools lastfm vim-gnome vim
+aptitude install git-core gitg gitk git-doc zsh firefox xmonad dwm-tools lastfm vim-gnome vim openvpn
 
