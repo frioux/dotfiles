76fc5f3bf0bca3bf75eb58527e1a410f68cd5375
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-01-14 00:29:02 -0600

    you need a mouse to browse the web

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 
 rm ~/.xmonad ~/bin/showdm ~/bin/eval ~/.Xdefaults ~/.vim ~/.zshrc ~/.vimrc\
    ~/.screenrc ~/.irssi ~/.gitconfig ~/.config/terminator/config\
-   ~/passwords.kdb ~/.xsession ~/bin/wrap-git ~/.pentadactylrc ~/.zsh \
+   ~/passwords.kdb ~/.xsession ~/bin/wrap-git ~/.zsh \
    ~/.tmux.conf ~/.dzil ~/bin/git-amend-file-split -Rf
 
 ln -s "$(pwd)/bin/eval" ~/bin/eval
@@ -29,7 +29,6 @@ ln -s "$(pwd)/zshrc" ~/.zshrc
 ln -s "$(pwd)/zsh" ~/.zsh
 ln -s "$(pwd)/passwords.kdb" ~/passwords.kdb
 ln -s "$(pwd)/xsession" ~/.xsession
-ln -s "$(pwd)/pentadactylrc" ~/.pentadactylrc
 ln -s "$(pwd)/tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/dzil" ~/.dzil
 
