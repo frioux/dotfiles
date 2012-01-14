6036e282b743876a117c5733e37655941b6c7282
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-01-14 00:43:48 -0600

    I can't even find a reference to this in the repo

--- install.sh
+++ install.sh
@@ -8,12 +8,11 @@ if [[ ! -x ~/.config/terminator ]]; then
    mkdir -p ~/.config/terminator
 fi
 
-rm ~/.xmonad ~/bin/showdm ~/bin/eval ~/.Xdefaults ~/.vim ~/.zshrc ~/.vimrc\
+rm ~/.xmonad ~/bin/showdm ~/.Xdefaults ~/.vim ~/.zshrc ~/.vimrc\
    ~/.screenrc ~/.irssi ~/.gitconfig ~/.config/terminator/config\
    ~/passwords.kdb ~/.xsession ~/bin/wrap-git ~/.zsh \
    ~/.tmux.conf ~/.dzil ~/bin/git-amend-file-split -Rf
 
-ln -s "$(pwd)/bin/eval" ~/bin/eval
 ln -s "$(pwd)/bin/showdm" ~/bin/showdm
 ln -s "$(pwd)/bin/wrap-git" ~/bin/wrap-git
 ln -s "$(pwd)/bin/git-amend-file-split" ~/bin/git-amend-file-split
