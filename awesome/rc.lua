168c9be63df30a0520c70a5e092d97f4c654d92b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-06-30 09:15:04 -0500

    now it acts almost exactly like xmonad

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -50,7 +50,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
