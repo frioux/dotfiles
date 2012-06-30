0e449c084953d527242684301832709818db432d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-06-30 11:26:31 -0500

    handy

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -375,3 +375,4 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+-- vim: foldmethod=marker
