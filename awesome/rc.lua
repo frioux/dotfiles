0c83f5114d7c4b170d76992b088da7e16571998b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-07-01 09:52:58 -0500

    seems like awesome.restart is just pure bs

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -228,7 +228,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-    awful.key({ modkey, "Control" }, "r", awesome.restart),
+    awful.key({ modkey, "Control" }, "r", awful.util.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
