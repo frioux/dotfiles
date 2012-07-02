3d89ea66ab15628a233e76e9b5f0f306ae07ad2c
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-07-02 17:20:00 -0500

    start work on xmonad-ish multihead

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -315,7 +315,10 @@ globalkeys = awful.util.table.join(
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show({keygrabber=true}) end),
+    awful.key({ modkey,           }, "w", function () awful.screen.focus(1) end),
+    --awful.key({ modkey, "Shift"   }, "w", function () awful.screen.focus(1) end),
+    awful.key({ modkey,           }, "e", function () awful.screen.focus(2) end),
+    --awful.key({ modkey, "Shift"   }, "e", function () awful.screen.focus(2) end),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
