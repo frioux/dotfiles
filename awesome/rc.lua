474ac6a1845b0e63ac2ada34b179a9e158a2b9ae
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-07-03 16:28:17 -0500

    get rid of silly tiling modes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -62,15 +62,7 @@ layouts =
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.floating,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    awful.layout.suit.tile.top
 }
 -- }}}
 
