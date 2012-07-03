ea4744adce977ffba0c1261d0c3fbff9181ac6e0
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-07-03 09:17:21 -0500

    go away gaps.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -452,6 +452,7 @@ awful.rules.rules = {
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
+                     size_hints_honor = false,
                      buttons = clientbuttons } },
     { rule = { class = "MPlayer" },
       properties = { floating = true } },
