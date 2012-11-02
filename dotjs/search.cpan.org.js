4c294aca50a065b4f408c123d075b61eb16d18b3
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-11-02 15:54:11 -0500

    more correctly convert links in s.c.o

--- dotjs/search.cpan.org.js
+++ dotjs/search.cpan.org.js
@@ -1,6 +1,17 @@
+(function() {
+var author_path = new RegExp('^/~([^/]+)/(.*)$');
+
 $('a').each(function(i,x){
    var obj = $(this);
    var href = obj.attr('href');
-   var re = new RegExp('^/~([^/]+)/(.*)$');
-   this.href = href.replace(re, 'https://metacpan.org/module/$1/$2');
+
+   this.href = href.replace(author_path, function(match, author, path) {
+      var middle = path.match(/\/$/)
+        ? 'release'
+        : 'module'
+      ;
+
+      return("http://metacpan.org/" + middle + "/" + author.toUpperCase() + '/' + path)
+   });
 })
+})()
