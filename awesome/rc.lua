51c3503eb7801ae39448a9a27b74d0819ce55f1d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-12-15 11:28:07 -0600

    remove menu (that I never use)

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -408,7 +408,6 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
