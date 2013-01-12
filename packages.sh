70d96709d6675b097f4731fcf052038f99496a2d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-01-12 14:16:05 -0600

    less manual deps

--- packages.sh
+++ packages.sh
@@ -15,7 +15,7 @@ apt-get --no-install-recommends install \
    libcurl4-openssl-dev                 \
    libgmime-2.6-dev                     \
    libncursesw5-dev                     \
-   libpam-yubico                        \
+   libpam0g-dev                         \
    libtalloc-dev                        \
    libtokyocabinet-dev                  \
    libxapian-dev                        \
