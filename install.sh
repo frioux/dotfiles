7226ed51cdb43dc4deb1a9b448a13375fd3ab1da
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-03-29 23:26:19 -0500

    do not link volitile file

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 function _mkdir { if [[ ! -d $1 ]]; then mkdir -p $1; fi }
 function link-file { _mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
+function copy-file { _mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
 
 link-file awesome ~/.config/awesome
 link-file dotjs ~/.js
 link-file ssh/config ~/.ssh/config
 link-file ssh/authorized_keys ~/.ssh/authorized_keys
 link-file terminator_config ~/.config/terminator/config
-link-file keepassx_config.ini ~/.config/keepassx/config.ini
+copy-file keepassx_config.ini ~/.config/keepassx/config.ini
 link-file install.sh .git/hooks/post-checkout
 link-file install.sh .git/hooks/post-merge
 link-file xsession ~/.xinitrc
