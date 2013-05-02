c4b7117d08dff98e9c059c127330e6c17f02cfab
Benjamin Kober <kober@optisense.com>
2013-05-02 22:56:07 +0200

    Updated freedesktop menu.

--- freedesktop/menu.lua
+++ freedesktop/menu.lua
@@ -1,81 +1,97 @@
 -- Grab environment
 local utils = require("freedesktop.utils")
 local io = io
-local ipairs = ipairs
+local string = string
 local table = table
 local os = os
+local ipairs = ipairs
+local pairs = pairs
 
 module("freedesktop.menu")
 
--- TODO: Parameter mit Pfadnamen uebergeben, damit das richtige Verzeichnis durchsucht werden kann
-function new()
-	-- the categories and their synonyms where shamelessly copied from lxpanel
-	-- source code.
-	local programs = {}
-	programs['AudioVideo'] = {}
-	--programs['Audio'] = {}
-	--programs['Video'] = {}
-	programs['Development'] = {}
-	--programs['Education'] = {}
-	--programs['Game'] = {}
-	programs['Graphics'] = {}
-	programs['Network'] = {}
-	programs['Office'] = {}
-	--programs['Settings'] = {}
-	programs['System'] = {}
-	programs['Utility'] = {}
-	programs['Other'] = {}
+all_menu_dirs = {
+    '/usr/share/applications/',
+    '/usr/local/share/applications/',
+    '~/.local/share/applications/'
+}
 
+show_generic_name = false
 
-	for i, program in ipairs(utils.parse_dir('/usr/share/applications/')) do
+--- Create menus for applications
+-- @param menu_dirs A list of application directories (optional).
+-- @return A prepared menu w/ categories
+function new(arg)
+    -- the categories and their synonyms where shamelessly copied from lxpanel
+    -- source code.
+    local programs = {}
+    local config = arg or {}
 
-		-- check whether to include in the menu
-		if program.show and program.Name and program.cmdline then
-			local target_category = nil
-			if program.categories then
-				for _, category in ipairs(program.categories) do
-					if programs[category] then
-						target_category = category
-						break
-					end
-				end
-			end
-			if not target_category then
-				target_category = 'Other'
-			end
-			if target_category then
-				table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
-			end
-		end
+    programs['AudioVideo'] = {}
+    programs['Development'] = {}
+    programs['Education'] = {}
+    programs['Game'] = {}
+    programs['Graphics'] = {}
+    programs['Network'] = {}
+    programs['Office'] = {}
+    programs['Settings'] = {}
+    programs['System'] = {}
+    programs['Utility'] = {}
+    programs['Other'] = {}
 
-	end
+    for i, dir in ipairs(config.menu_dirs or all_menu_dirs) do
+        local entries = utils.parse_desktop_files({dir = dir})
+        for j, program in ipairs(entries) do
+            -- check whether to include in the menu
+            if program.show and program.Name and program.cmdline then
+                if show_generic_name and program.GenericName then
+                    program.Name = program.Name .. ' (' .. program.GenericName .. ')'
+                end
+                local target_category = nil
+                if program.categories then
+                    for _, category in ipairs(program.categories) do
+                        if programs[category] then
+                            target_category = category
+                            break
+                        end
+                    end
+                end
+                if not target_category then
+                    target_category = 'Other'
+                end
+                if target_category then
+                    table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
+                end
+            end
+        end
+    end
 
-	local menu = {
-		{ "Accessories", programs["Utility"], utils.lookup_icon({ icon = 'applications-accessories.png' }) },
-		{ "Development", programs["Development"], utils.lookup_icon({ icon = 'applications-development.png' }) },
-		{ "Education", programs["Education"], utils.lookup_icon({ icon = 'applications-science.png' }) },
-		{ "Games", programs["Game"], utils.lookup_icon({ icon = 'applications-games.png' }) },
-		{ "Graphics", programs["Graphics"], utils.lookup_icon({ icon = 'applications-graphics.png' }) },
-		{ "Internet", programs["Network"], utils.lookup_icon({ icon = 'applications-internet.png' }) },
-		{ "Multimedia", programs["AudioVideo"], utils.lookup_icon({ icon = 'applications-multimedia.png' }) },
-		{ "Office", programs["Office"], utils.lookup_icon({ icon = 'applications-office.png' }) },
-		{ "Other", programs["Other"], utils.lookup_icon({ icon = 'applications-other.png' }) },
-		{ "Settings", programs["Settings"], utils.lookup_icon({ icon = 'applications-utilities.png' }) },
-		{ "System Tools", programs["System"], utils.lookup_icon({ icon = 'applications-system.png' }) },
-	}
+    -- sort each submenu alphabetically case insensitive
+    for k, v in pairs(programs) do
+        table.sort(v, function(a, b) return a[1]:lower() < b[1]:lower() end)
+    end
 
-	-- Removing empty entries from menu
-	local bad_indexes = {}
-	for index , item in ipairs(menu) do
-		if not item[2] then
-			table.insert(bad_indexes, index)
-		end
-	end
-	table.sort(bad_indexes, function (a,b) return a > b end)
-	for _, index in ipairs(bad_indexes) do
-		table.remove(menu, index)
-	end
+    local menu = {
+        { "Accessories", programs["Utility"], utils.lookup_icon({ icon = 'applications-accessories.png' }) },
+        { "Development", programs["Development"], utils.lookup_icon({ icon = 'applications-development.png' }) },
+        { "Education", programs["Education"], utils.lookup_icon({ icon = 'applications-science.png' }) },
+        { "Games", programs["Game"], utils.lookup_icon({ icon = 'applications-games.png' }) },
+        { "Graphics", programs["Graphics"], utils.lookup_icon({ icon = 'applications-graphics.png' }) },
+        { "Internet", programs["Network"], utils.lookup_icon({ icon = 'applications-internet.png' }) },
+        { "Multimedia", programs["AudioVideo"], utils.lookup_icon({ icon = 'applications-multimedia.png' }) },
+        { "Office", programs["Office"], utils.lookup_icon({ icon = 'applications-office.png' }) },
+        { "Other", programs["Other"], utils.lookup_icon({ icon = 'applications-other.png' }) },
+        { "Settings", programs["Settings"], utils.lookup_icon({ icon = 'preferences-desktop.png' }) },
+        { "System Tools", programs["System"], utils.lookup_icon({ icon = 'applications-system.png' }) },
+    }
 
-	return menu
+    -- Removing empty entries from menu
+    local cleanedMenu  = {}
+    for index, item in ipairs(menu) do
+	itemTester = item[2]
+        if itemTester[1] then
+            table.insert(cleanedMenu, item)
+        end
+    end
+
+    return cleanedMenu
 end
-
--- freedesktop/utils.lua
+++ freedesktop/utils.lua
@@ -1,8 +1,11 @@
 -- Grab environment
 
 local io = io
+local os = os
 local table = table
+local type = type
 local ipairs = ipairs
+local pairs = pairs
 
 module("freedesktop.utils")
 
@@ -10,22 +13,43 @@ terminal = 'xterm'
 
 icon_theme = nil
 
-all_icon_sizes = { 
-	'128x128' ,
-	'96x96', 
-	'72x72', 
-	'64x64', 
-	'48x48', 
-	'36x36', 
-	'32x32', 
-	'24x24', 
-	'22x22', 
-	'16x16' 
+all_icon_sizes = {
+    '128x128',
+    '96x96',
+    '72x72',
+    '64x64',
+    '48x48',
+    '36x36',
+    '32x32',
+    '24x24',
+    '22x22',
+    '16x16'
 }
+all_icon_types = {
+    'apps',
+    'actions',
+    'devices',
+    'places',
+    'categories',
+    'status',
+    'mimetypes'
+}
+all_icon_paths = { os.getenv("HOME") .. '/.icons/', '/usr/share/icons/' }
 
 icon_sizes = {}
 
-local function file_exists(filename)
+local mime_types = {}
+
+function get_lines(...)
+    local f = io.popen(...)
+    return function () -- iterator
+        local data = f:read()
+        if data == nil then f:close() end
+        return data
+    end
+end
+
+function file_exists(filename)
     local file = io.open(filename, 'r')
     local result = (file ~= nil)
     if result then
@@ -40,39 +64,92 @@ function lookup_icon(arg)
         return arg.icon
     else
         local icon_path = {}
+        local icon_themes = {}
         local icon_theme_paths = {}
-        if icon_theme then
-            table.insert(icon_theme_paths, '/usr/share/icons/' .. icon_theme .. '/')
+        if icon_theme and type(icon_theme) == 'table' then
+            icon_themes = icon_theme
+        elseif icon_theme then
+            icon_themes = { icon_theme }
+        end
+        for i, theme in ipairs(icon_themes) do
+            for j, path in ipairs(all_icon_paths) do
+                table.insert(icon_theme_paths, path .. theme .. '/')
+            end
             -- TODO also look in parent icon themes, as in freedesktop.org specification
         end
         table.insert(icon_theme_paths, '/usr/share/icons/hicolor/') -- fallback theme cf spec
 
-		local isizes = icon_sizes
+        local isizes = icon_sizes
         for i, sz in ipairs(all_icon_sizes) do
-			table.insert(isizes, sz)
-		end
+            table.insert(isizes, sz)
+        end
 
         for i, icon_theme_directory in ipairs(icon_theme_paths) do
             for j, size in ipairs(arg.icon_sizes or isizes) do
-                table.insert(icon_path, icon_theme_directory .. size .. '/apps/')
-                table.insert(icon_path, icon_theme_directory .. size .. '/actions/')
-                table.insert(icon_path, icon_theme_directory .. size .. '/devices/')
-                table.insert(icon_path, icon_theme_directory .. size .. '/places/')
-                table.insert(icon_path, icon_theme_directory .. size .. '/categories/')
-                table.insert(icon_path, icon_theme_directory .. size .. '/status/')
+                for k, icon_type in ipairs(all_icon_types) do
+                    table.insert(icon_path, icon_theme_directory .. size .. '/' .. icon_type .. '/')
+                end
             end
         end
         -- lowest priority fallbacks
         table.insert(icon_path,  '/usr/share/pixmaps/')
         table.insert(icon_path,  '/usr/share/icons/')
+        table.insert(icon_path,  '/usr/share/app-install/icons/')
 
         for i, directory in ipairs(icon_path) do
             if (arg.icon:find('.+%.png') or arg.icon:find('.+%.xpm')) and file_exists(directory .. arg.icon) then
                 return directory .. arg.icon
-            elseif file_exists(directory .. arg.icon .. '.xpm') then
-                return directory .. arg.icon .. '.xpm'
             elseif file_exists(directory .. arg.icon .. '.png') then
                 return directory .. arg.icon .. '.png'
+            elseif file_exists(directory .. arg.icon .. '.xpm') then
+                return directory .. arg.icon .. '.xpm'
+            end
+        end
+    end
+end
+
+function lookup_file_icon(arg)
+    load_mime_types()
+
+    local extension = arg.filename:match('%a+$')
+    local mime = mime_types[extension] or ''
+    local mime_family = mime:match('^%a+') or ''
+
+    -- possible icons in a typical gnome theme (i.e. Tango icons)
+    local possible_filenames = {
+        mime,
+        'gnome-mime-' .. mime,
+        mime_family,
+        'gnome-mime-' .. mime_family,
+        extension
+    }
+
+    for i, filename in ipairs(possible_filenames) do
+        local icon = lookup_icon({icon = filename, icon_sizes = (arg.icon_sizes or all_icon_sizes)})
+        if icon then
+            return icon
+        end
+    end
+
+    -- If we don't find ad icon, then pretend is a plain text file
+    return lookup_icon({ icon = 'txt', icon_sizes = arg.icon_sizes or all_icon_sizes })
+end
+
+--- Load system MIME types
+-- @return A table with file extension <--> MIME type mapping
+function load_mime_types()
+    if #mime_types == 0 then
+        for line in io.lines('/etc/mime.types') do
+            if not line:find('^#') then
+                local parsed = {}
+                for w in line:gmatch('[^%s]+') do
+                    table.insert(parsed, w)
+                end
+                if #parsed > 1 then 
+                    for i = 2, #parsed do
+                        mime_types[parsed[i]] = parsed[1]:gsub('/', '-')
+                    end
+                end
             end
         end
     end
@@ -82,39 +159,45 @@ end
 -- @param file The .desktop file
 -- @param requested_icon_sizes A list of icon sizes (optional). If this list is given, it will be used as a priority list for icon sizes when looking up for icons. If you want large icons, for example, you can put '128x128' as the first item in the list.
 -- @return A table with file entries.
-function parse(file, requested_icon_sizes)
-    local program = { show = true, file = file }
-    for line in io.lines(file) do
+function parse_desktop_file(arg)
+    local program = { show = true, file = arg.file }
+    for line in io.lines(arg.file) do
         for key, value in line:gmatch("(%w+)=(.+)") do
             program[key] = value
         end
     end
 
+    -- Don't show the program if NoDisplay is true
     -- Only show the program if there is not OnlyShowIn attribute
     -- or if it's equal to 'awesome'
-    if program.OnlyShowIn ~= nil and program.OnlyShowIn ~= "awesome" then
+    if program.NoDisplay == "true" or program.OnlyShowIn ~= nil and program.OnlyShowIn ~= "awesome" then
         program.show = false
     end
 
     -- Look up for a icon.
     if program.Icon then
-        program.icon_path = lookup_icon({ icon = program.Icon, icon_sizes = (requested_icon_sizes or all_icon_sizes) })
+        program.icon_path = lookup_icon({ icon = program.Icon, icon_sizes = (arg.icon_sizes or all_icon_sizes) })
+        if program.icon_path ~= nil and not file_exists(program.icon_path) then
+           program.icon_path = nil
+        end
     end
 
     -- Split categories into a table.
     if program.Categories then
         program.categories = {}
-        for category in program.Categories:gfind('[^;]+') do
+        for category in program.Categories:gmatch('[^;]+') do
             table.insert(program.categories, category)
         end
     end
 
     if program.Exec then
         local cmdline = program.Exec:gsub('%%c', program.Name)
-        cmdline = cmdline:gsub('%%[fuFU]', '')
+        cmdline = cmdline:gsub('%%[fmuFMU]', '')
         cmdline = cmdline:gsub('%%k', program.file)
         if program.icon_path then
             cmdline = cmdline:gsub('%%i', '--icon ' .. program.icon_path)
+        else
+            cmdline = cmdline:gsub('%%i', '')
         end
         if program.Terminal == "true" then
             cmdline = terminal .. ' -e ' .. cmdline
@@ -129,11 +212,44 @@ end
 -- @param dir The directory.
 -- @param icons_size, The icons sizes, optional.
 -- @return A table with all .desktop entries.
-function parse_dir(dir, icon_sizes)
+function parse_desktop_files(arg)
     local programs = {}
-    local files = io.popen('find '.. dir ..' -maxdepth 1 -name "*.desktop"'):lines()
+    local files = get_lines('find '.. arg.dir ..' -name "*.desktop" 2>/dev/null')
     for file in files do
-        table.insert(programs, parse(file, icon_sizes))
+        arg.file = file
+        table.insert(programs, parse_desktop_file(arg))
     end
     return programs
 end
+
+--- Parse a directory files and subdirs
+-- @param dir The directory.
+-- @param icons_size, The icons sizes, optional.
+-- @return A table with all .desktop entries.
+function parse_dirs_and_files(arg)
+    local files = {}
+    local paths = get_lines('find '..arg.dir..' -maxdepth 1 -type d')
+    for path in paths do
+        if path:match("[^/]+$") then
+            local file = {}
+            file.filename = path:match("[^/]+$")
+            file.path = path
+            file.show = true
+            file.icon = lookup_icon({ icon = "folder", icon_sizes = (arg.icon_sizes or all_icon_sizes) })
+            table.insert(files, file)
+        end
+    end
+    local paths = get_lines('find '..arg.dir..' -maxdepth 1 -type f')
+    for path in paths do
+        if not path:find("%.desktop$") then
+            local file = {}
+            file.filename = path:match("[^/]+$")
+            file.path = path
+            file.show = true
+            file.icon = lookup_file_icon({ filename = file.filename, icon_sizes = (arg.icon_sizes or all_icon_sizes) })
+            table.insert(files, file)
+        end
+    end
+    return files
+end
+
