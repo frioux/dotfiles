4a99de4e3963338045d9c9fe913af66f79fa9422
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-08-10 17:00:55 -0500

    use wes' new roller

--- install.sh
+++ install.sh
@@ -19,6 +19,8 @@ link-file install.sh .git/hooks/post-merge
 link-file xsession ~/.xinitrc
 link-file gpg.conf ~/.gnupg/gpg.conf
 link-file todo ~/.todo/config
+link-file ww-roller.pl/ww_roll.pl ~/bin/ww-roll.pl
+chmod +x ~/bin/ww-roll.pl
 source ~/.smartcd/lib/core/arrays
 source ~/.smartcd/lib/core/varstash
 source ~/.smartcd/lib/core/smartcd
