1b82b219b2117a438745007b8ce82bb0ec206682
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-10-29 12:24:10 -0500

    stop clobbering _mkdir, the mkdir completer

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@
 echo "running rc installer"
 trap "echo 'done installing'" USR1
 
-function _mkdir { if [[ ! -d $1 ]]; then mkdir -p $1; fi }
-function link-file { _mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
-function copy-file { _mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
+function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
+function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
 
 perl install-cronjobs.pl
 link-file awesome ~/.config/awesome
