1aa892de76fd3e5008975a34413a47fcefa54521
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-11-09 12:39:46 -0600

    better dzil minting (thanks rjbs!)

--- dzil/profiles/default/Module.pm
+++ dzil/profiles/default/Module.pm
@@ -3,6 +3,20 @@ package {{ $name }};
 use strict;
 use warnings;
 
-# ABSTRACT - FILL THIS OUT
+# ABSTRACT: FILL THIS OUT
 
 1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+=cut
