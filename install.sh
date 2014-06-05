b7adc62adc4b079910671b1e3dd96c5a57e70415
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-06-05 14:45:09 -0500

    this was just a bad idea

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ echo "running rc installer"
 function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
 function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
 
-perl install-cronjobs.pl
 link-file awesome ~/.config/awesome
 link-file dotjs ~/.js
 link-file ssh/config ~/.ssh/config
