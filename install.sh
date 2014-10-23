59b21491c192ba3704f680ab865f4a78b6ea1057
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-10-23 09:14:48 -0500

    no errors when running install.sh fresh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "running rc installer"
 
+function __mkdir { if [[ ! -d $1 ]]; then mkdir -p $1; fi }
 function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
 function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
 
