098cb64345485c75f706a408f071d3d4bc250182
Arthur Axel 'fREW' Schmidt <git@frew.co>
2018-09-01 23:40:18 -0700

    Replace dmenu with rofi

--- packages.x11.sh
+++ packages.x11.sh
@@ -4,7 +4,7 @@ sudo snap install keepassxc
 
 exec apt-get --no-install-recommends    \
                              install -y \
-   dzen2                                \
+   rofi                                 \
    feh                                  \
    firefox                              \
    fonts-lohit-knda                     \
