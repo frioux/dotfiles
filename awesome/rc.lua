41aa61857fdd137ce29791fb4dfa11b7f846e39f
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-05-22 22:20:15 -0700

    Automatically route a few windows

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -620,13 +620,11 @@ awful.rules.rules = {
       }, properties = { floating = true }},
 
     -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
-    },
+    { rule_any = {type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox"   }, properties = { tag = "2" } },
+    { rule = { class = "keepassxc" }, properties = { tag = "8" } },
+    { rule = { class = "Signal"    }, properties = { tag = "9" } },
 }
 -- }}}
 
