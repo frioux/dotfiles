89333f080384931d6d17a50fed7984e928bbfb6e
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-05-22 22:05:42 -0700

    No border for single window

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -653,6 +653,16 @@ function awful.ewmh.activate(c)
     end
 end
 client.connect_signal("request::activate", awful.ewmh.activate)
+
+screen.connect_signal("arrange", function(s)
+    for _, c in pairs(s.clients) do
+        if #s.tiled_clients == 1 and c.floating == false then
+            c.border_width = 0
+        elseif #s.tiled_clients > 1 then
+            c.border_width = beautiful.border_width
+        end
+    end
+end)
 -- }}}
 
 -- vim: foldmethod=marker
