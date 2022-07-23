4a087547326cc61661f5840c73e033a33df4444d
Arthur Axel 'fREW' Schmidt <git@frew.co>
2022-07-23 10:33:39 -0700

    update wallpaper on click

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -274,6 +274,9 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
+    awful.button({ }, 1, function ()
+        awful.spawn("sh -c 'feh --bg-max $(find ~/Dropbox/Pictures/wallpaper/ | shuf | tail -2) ~/Dropbox/Pictures/wallpaper/marble.jpg'")
+    end),
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
